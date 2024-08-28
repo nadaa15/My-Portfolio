933f8f9657564af71189875c8787ddb217e26106
Your Name <na835126@gmail.com>
2024-08-28 06:56:34 +0300

    first commit

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import img from "../assets/images/error.jpg";
 
 export default function NotFound() {
+  const [first, setfirst] = useState("");
+  useEffect(() => {}, []);
+
   return (
-    <div>NotFound</div>
-  )
+    <>
+      <div className="container flex justify-center items-center my-[77px]">
+        <img className="w-1/2" src={img} alt="404 image" />
+      </div>
+    </>
+  );
 }
